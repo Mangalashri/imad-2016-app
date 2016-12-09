cf0e58ef988cdf5d3c78e22e21846ceb76ffa3b9
Mangalashri <rmangalashri@gmail.com>
2016-12-09 23:42:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  
 });
 
 app.get('/article-one',function(req,res){
-  res.send('Article 1');
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two',function(req,res){
   res.send('Article 2');
