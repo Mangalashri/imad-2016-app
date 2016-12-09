de2952b1a6eadb20857bdf90d153dee9bb9ca157
Mangalashri <rmangalashri@gmail.com>
2016-12-10 00:20:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
- res.send(create(articles[articleName]));
+ res.send(create(ars[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
