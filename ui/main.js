fdb7215030e45e9115c775309022dc368966e8e6
Mangalashri <rmangalashri@gmail.com>
2016-12-10 12:17:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 var ele=document.getElementById('id1');
-ele.innerHTML='babe';
+ele.innerHTML='heyyy';
 var ele1=document.getElementById('imgid');
 ele1.onclick = function(){
     ele1.style.marginLeft='1000px';
